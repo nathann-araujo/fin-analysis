8ea9a28b38685dca973ad565cb8170ebaf96d92e
nathann <n47h4nn@gmail.com>
2022-04-11 20:29:31 -0300

    first commit

---
+++ src/test/java/br/com/nn/fin_analysis/FinAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nn.fin_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
