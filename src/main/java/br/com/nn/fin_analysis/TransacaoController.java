3aa6c32d53cc29889cb445524dbd931c3e4ece68
nathann <n47h4nn@gmail.com>
2022-04-12 14:47:30 -0300

    feat: add read rows of csv file

--- src/main/java/br/com/nn/fin_analysis/TransacaoController.java
+++ src/main/java/br/com/nn/fin_analysis/TransacaoController.java
@@ -1,5 +1,11 @@
 package br.com.nn.fin_analysis;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +25,16 @@ public class TransacaoController {
 	public ModelAndView postTransacao(@RequestParam("file") MultipartFile file) {
 		System.out.println("Nome: " + file.getOriginalFilename());
 		System.out.println("Tamanho: " + file.getSize()/1E6 + "MB"); 
+		try {
+			InputStream inputStream = file.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			Stream<String> lines = bufferedReader.lines();
+			lines.forEach(System.out::println);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return new ModelAndView("importar-transacoes");
 	}
 }
