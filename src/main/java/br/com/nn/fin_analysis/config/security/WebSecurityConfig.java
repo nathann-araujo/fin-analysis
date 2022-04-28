f2f365b714b660be3d764a67f56f9d7f4955a3a2
nathann <n47h4nn@gmail.com>
2022-04-28 14:48:20 -0300

    feat: add registered users list

--- src/main/java/br/com/nn/fin_analysis/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/nn/fin_analysis/config/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.nn.fin_analysis.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,12 +14,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("admin@email.com.br").password(getEncoder().encode("123999")).roles("ADMIN")
+			.and()
+			.withUser("teste@gmail.com").password(getEncoder().encode("123")).roles("USER");
+	}
+	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
 		.mvcMatchers("/signup").permitAll()
+		.mvcMatchers("/usuarios").authenticated()
 		.and().csrf().disable()
 		.headers().frameOptions().disable()
+		.and().formLogin().defaultSuccessUrl("/usuarios")
 		;
 	}
 	
---
+++ src/main/java/br/com/nn/fin_analysis/dto/UsuarioDto.java
@@ -0,0 +1,44 @@
+package br.com.nn.fin_analysis.dto;
+
+import java.util.Objects;
+
+public class UsuarioDto {
+	private Long id;
+	private String nome;
+	private String email;
+	
+	public UsuarioDto(Long id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UsuarioDto other = (UsuarioDto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
