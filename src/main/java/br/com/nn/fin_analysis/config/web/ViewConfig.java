3b8fd572f76ba6e49e5d2f40ed881cf1a19a3031
nathann <n47h4nn@gmail.com>
2022-05-01 23:19:11 -0300

    fix: change default index mapping

---
+++ src/main/java/br/com/nn/fin_analysis/config/web/ViewConfig.java
@@ -0,0 +1,16 @@
+package br.com.nn.fin_analysis.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ViewConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/transacoes");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	}
+}
